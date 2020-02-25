b91a535a47c2925813006d0d5c3e01171684ca5f
YunaH <YunaH503@gmail.com>
2020-02-25 17:34:31 -0500

    Modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
 
   resources :spells, except: %i[new edit]
+  resources :users, except: %i[new edit]
   # RESTful routes
   # resources :examples, except: %i[new edit]
 
