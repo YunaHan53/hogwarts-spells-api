cfbc9b6659a166ea6f3b3d222600fa6a3c33ba69
YunaH <YunaH503@gmail.com>
2020-02-26 17:00:30 -0500

    Added working curl-scripts for spell
    Added editable method in spell_serializer
    Added create, show, update & destroy curl scripts.

--- app/serializers/spell_serializer.rb
+++ app/serializers/spell_serializer.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class SpellSerializer < ActiveModel::Serializer
-  attributes :id, :name, :category, :description, :fatality, :user
+  attributes :id, :name, :category, :description, :fatality, :user, :editable
+
+  def editable
+    scope == object.user
+  end
+
 end
